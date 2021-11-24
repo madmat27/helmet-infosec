d4a39cc569b5c9048ebceb6d915114eecb52d750
Mariana S. Mazi <mscmazi@gmail.com>
2021-11-24 17:19:01 +0200

    Final settings with comments for securing the site

--- myApp.js
+++ myApp.js
@@ -1,53 +1,43 @@
 const express = require('express');
 const app = express();
 const helmet = require('helmet');
+//Set maxAge variable:
+var ninetyDaysInSeconds = 90 * 24 * 60 * 60;
 
-app.use(helmet());
+// Hide X-Powered-By: Express
+app.use(helmet.hidePoweredBy());
+// Mitigate the Risk of Clickjacking
+app.use(helmet.frameguard({
+    action: "deny"
+  })
+);
+// Mitigate the Risk of Cross Site Scripting (XSS) Attacks
+app.use(helmet.xssFilter());
+// Avoid Inferring the Response MIME Type
+app.use(helmet.noSniff());
+// Prevent IE from Opening Untrusted HTML
+app.use(helmet.ieNoOpen());
+// Ask Browsers to Access Your Site via HTTPS Only
+app.use(helmet.hsts({
+    maxAge: ninetyDaysInSeconds,
+    force: 'true'
+  })
+);
+// Disable DNS Prefetching
+app.use(helmet.dnsPrefetchControl());
+// Disable Client-Side Caching
+app.use(helmet.noCache());
+// Set a Content Security Policy
+app.use(helmet.contentSecurityPolicy({
+    useDefaults: false,
+    directives:{
+      defaultSrc: ["'self'"],
+      scriptSrc: ["'self'", 'trusted-cdn.com']
+    },
+  })
+)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+/***********************************************/
 
 module.exports = app;
 const api = require('./server.js');
